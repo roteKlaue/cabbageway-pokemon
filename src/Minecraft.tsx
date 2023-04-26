1cf88a65ac6b4404bbd6b6464c86fb43737b8a37
roteKlaue <87981510+roteKlaue@users.noreply.github.com>
2023-04-26 09:21:20 +0200

    added super important component

---
+++ src/Minecraft.tsx
@@ -0,0 +1,12 @@
+function Minecraft () {
+    return (<>
+        <div>
+            minecraft
+        </div>
+        <div>
+            minecraft
+        </div>
+    </>)
+}
+
+export default Minecraft;
